69d41019b52bb57be122c1e91ceec40c13b2b842
Hunter McKinney <hmckinney92@gmail.com>
2023-01-14 15:46:01 -0600

    Part 1 finished

---
+++ juicebox/index.js
@@ -0,0 +1,12 @@
+async function testDB() {
+    try {
+      client.connect();
+  
+      const { rows } = await client.query(`SELECT * FROM users;`);
+      console.log(rows);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      client.end();
+    }
+  }
